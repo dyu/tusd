1846715b5a7c75807f651e02eba90b76ce4da166
dyu <david.yu.ftw@gmail.com>
2017-12-07 01:56:18 +0800

    build

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# locate
+if [ ! -n "$BASH_SOURCE" ]; then
+    SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
+else
+    F=$BASH_SOURCE
+    while [ -h "$F" ]; do
+        F="$(readlink "$F")"
+    done
+    SCRIPT_DIR=`dirname "$F"`
+fi
+
+cd $SCRIPT_DIR
+
+go build cmd/tusd/main.go
